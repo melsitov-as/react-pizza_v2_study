0986aa9a0778330b022161ba4c4729542ae25df4
Alexander <mas2005@inbox.ru>
2023-02-09 13:45:54 +0300

    Создает реакт-компоненты

--- study-project/src/App.js
+++ study-project/src/App.js
@@ -1,9 +1,29 @@
+import React from "react";
+import "./scss/app.scss";
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+	return (
+		<div class="wrapper">
+			<Header />
+			<div class="content">
+				<div class="container">
+					<div class="content__top">
+						<Categories />
+						<Sort />
+					</div>
+					<h2 class="content__title">Все пиццы</h2>
+					<div class="content__items">
+						<PizzaBlock title="Чизбургер-пицца" price={500} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
