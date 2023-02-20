5021030be6ee03a55e7a614a547be4a48abcc692
Alexander <mas2005@inbox.ru>
2023-02-20 12:34:43 +0300

    Поправляет поиск, чтобы сохранялся фокус при очистке инпута поиска

--- study-project/src/components/Search/index.jsx
+++ study-project/src/components/Search/index.jsx
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { SearchContext } from "../../App";
 
 import styles from "./Search.module.scss";
 
 const Search = () => {
-	// Вытащим searchValue и setSearchValue из SearchContext
 	const { searchValue, setSearchValue } = useContext(SearchContext);
+	const inputRef = useRef();
+
+	const onClickClear = () => {
+		setSearchValue("");
+		inputRef.current.focus();
+	};
 
 	return (
 		<div className={styles.root}>
@@ -19,7 +24,10 @@ const Search = () => {
 					<path d="M29.71,28.29l-6.5-6.5-.07,0a12,12,0,1,0-1.39,1.39s0,.05,0,.07l6.5,6.5a1,1,0,0,0,1.42,0A1,1,0,0,0,29.71,28.29ZM14,24A10,10,0,1,1,24,14,10,10,0,0,1,14,24Z" />
 				</g>
 			</svg>
+			{/* useRef 
+			наша задача получить DOM-элемент инпута */}
 			<input
+				ref={inputRef}
 				className={styles.input}
 				type="text"
 				placeholder="Поиск пиццы..."
@@ -28,7 +36,7 @@ const Search = () => {
 			/>
 			{searchValue && (
 				<svg
-					onClick={() => setSearchValue("")}
+					onClick={() => onClickClear()}
 					className={styles.clearIcon}
 					viewBox="0 0 32 32"
 					xmlns="http://www.w3.org/2000/svg"
